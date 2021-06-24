f04fac2d12f507deeb3f6b1c9b4de4c14debc94f
Sean Moran <sean_r_moran@msn.com>
2021-06-23 23:30:05 -0400

    Added some scaffolding for the form

---
+++ app.js
@@ -0,0 +1,32 @@
+const {document} = globalThis;
+
+const element = document?.getElementById('notifications');
+
+import {useState} from 'react';
+import {render} from 'react-dom';
+import {post} from 'axios';
+
+render(<Notifications />, element);
+
+export default function Notifications() {
+  const [submitted, setSubmitted] = useState();
+  const [error, setError] = useState();
+
+  return <form onSubmit={requestNotifications}>
+  </form>;
+
+  async function requestNotifications(event) {
+    event.preventDefault();
+
+    setSubmitted();
+    setError();
+
+    try {
+      await post('https://6099a4760f5a13001721985c.mockapi.io/api/submit');
+    } catch(error) {
+      setError(true);
+    }
+
+    setSubmitted(true);
+  }
+}
